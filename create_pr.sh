229a7c0ec50b4978095695427f58d76e0327b400
Daniel Herrera <daniel.herrera@ibm.com>
2019-01-29 16:31:26 -0600

    changed code 2

--- create_pr.sh
+++ create_pr.sh
@@ -3,7 +3,7 @@ gh_token=$1
 random_string=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 git checkout -b branch-$random_string
-
+echo "Hello Im a new change"
 touch yadda-$random_string.out
 echo "yadda" >> yadda-$random_string.out
 git add yadda-$random_string.out
