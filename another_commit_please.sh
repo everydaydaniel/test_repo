84b657a00c52ffe4247281392235948aab2012f1
Daniel Herrera <daniel94@utexas.edu>
2019-03-26 17:02:26 -0500

    stream this please

--- another_commit_please.sh
+++ another_commit_please.sh
@@ -5,4 +5,4 @@ echo "yadda" >> yadda.out
 git add yadda.out
 random_string=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 git commit -m "randmon string for commit message $random_string"
-git push 
+git push origin master
